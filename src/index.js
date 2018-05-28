94f4fa4a2836e2947aab8f53f4283a184fbf7231
Bob Wei <bob.wei@linecorp.com>
2018-05-28 23:56:47 +0800

    Mv fn to createData

--- src/index.js
+++ src/index.js
@@ -1,41 +1,7 @@
-/* eslint-disable camelcase */
-import path from 'path';
-import fs from 'fs';
-import mkdirp from 'mkdirp';
-import async from 'async';
-import axios from 'axios';
-import R from 'ramda';
+import createData from 'modules/core/functions/createData';
 
-import getPhotos from 'modules/core/functions/getPhotos';
-
-const fn = ({
-  outputDir = path.resolve('tmp'),
-  photoOutputDir = path.join(outputDir, 'photos'),
-} = {}) => {
-  return Promise.resolve(require('modules/fixtures/data/timeline_media.json'))
-    .then(getPhotos)
-    .then(photos => {
-      console.log(`${photos.length} photos downloading...`);
-      return new Promise((resolve, reject) => {
-        const concurrency = 2;
-        const queue = async.queue(({ id, display_url }, callback) => {
-          return axios
-            .get(display_url, { responseType: 'stream' })
-            .then(R.path(['data']))
-            .then(stream => {
-              mkdirp.sync(photoOutputDir);
-              stream.pipe(
-                fs.createWriteStream(path.join(photoOutputDir, `${id}.jpg`)),
-              );
-              stream.on('end', callback);
-              stream.on('error', callback);
-            });
-        }, concurrency);
-        queue.push(photos);
-        queue.drain = error => (error ? reject(error) : resolve());
-      });
-    })
-    .catch(console.log);
+const fn = () => {
+  return Promise.all([createData()]);
 };
 
 if (require.main === module) {
---
+++ src/modules/core/functions/createData.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+import path from 'path';
+import fs from 'fs';
+import mkdirp from 'mkdirp';
+import async from 'async';
+import axios from 'axios';
+import R from 'ramda';
+
+import getPhotos from 'modules/core/functions/getPhotos';
+
+const fn = ({
+  outputDir = path.resolve('tmp'),
+  photoOutputDir = path.join(outputDir, 'photos'),
+} = {}) => {
+  return Promise.resolve(require('modules/fixtures/data/timeline_media.json'))
+    .then(getPhotos)
+    .then(photos => {
+      console.log(`${photos.length} photos downloading...`);
+      return new Promise((resolve, reject) => {
+        const concurrency = 2;
+        const queue = async.queue(({ id, display_url }, callback) => {
+          return axios
+            .get(display_url, { responseType: 'stream' })
+            .then(R.path(['data']))
+            .then(stream => {
+              mkdirp.sync(photoOutputDir);
+              stream.pipe(
+                fs.createWriteStream(path.join(photoOutputDir, `${id}.jpg`)),
+              );
+              stream.on('end', callback);
+              stream.on('error', callback);
+            });
+        }, concurrency);
+        queue.push(photos);
+        queue.drain = error => (error ? reject(error) : resolve());
+      });
+    });
+};
+
+export default fn;
